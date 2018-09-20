3dcdf32d62ee6a7faf95bb0ab15fe45aa9119c51
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-20 22:48:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -93,7 +93,26 @@ client.on("guildMemberAdd", (member) => {
 
 });
 
+client.on("message", message => {
     
+    if(message.content.startsWith(prefix + "server")) {
+        if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("**الأمر للاداره فقط **");
+const embed = new Discord.RichEmbed()
+        .setAuthor(message.guild.name, message.guild.iconURL)
+        .setColor("RANDOM")
+.setDescription(`**
+.addField("**👑__اونر السيرفر__**", '**Unknown**' ,true)
+.addField("**🆔__ايدي السيرفر__**", message.guild.id,true)
+.addField("**📅__تم إنشاؤها__**", message.guild.createdAt.toLocaleString(),true)
+.addField("**🌐__نوع السيرفر__**", message.guild.region,true)
+.addField("**👥__عدد الاعضاء__**", message.guild.memberCount`,true)
+.addField("**📝__الرومات الكتابية__**", message.guild.channels.filter(m => m.type === 'text').size`,true)
+.addField("**🎤__رومات الصوت__**", message.guild.channels.filter(m => m.type === 'voice').size`,true)
+.addField("**🔵__عدد الاعضاء الاونلاين__**", message.guild.members.filter(m=>m.presence.status == 'online').size`,true)
+** `)
+message.channel.send({embed:embed})
+    }
+});
 
 
   
