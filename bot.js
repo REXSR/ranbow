f4a46f8a203ce0a98d547bbf25e68b68d314fb74
REXSR <41483292+REXSR@users.noreply.github.com>
2018-10-12 17:06:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,9 +34,9 @@ client.on('message', message => {
  
               if(!message.channel.guild) return;
  
-    var prefix = "!";
+    var prefix = ".";
  
-    if(message.content.startsWith('!bc')) {
+    if(message.content.startsWith('.bc')) {
  
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
  
